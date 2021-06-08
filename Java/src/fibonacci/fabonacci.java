f193456aa0ca5b1bbfe4e735dd0f3682fa4d0516
Anuja <Anuja@DESKTOP-NH0JUEA>
2021-06-08 14:11:55 +0530

    First Commit

---
+++ Java/src/fibonacci/fabonacci.java
@@ -0,0 +1,25 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class fabonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, f1 = 0, f2 = 0, f3 = 1 ;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter value of n:");
+        n = s.nextInt();
+        System.out.print("Fibonacci Series:");
+        for(int i = 1; i <= n; i++)
+        {
+        	f1 = f2;
+           f2 = f3;
+           f3 = f1 + f2;
+           System.out.print(f1+" ");
+
+
+        }
+	}
+
+}
